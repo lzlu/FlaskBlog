b5d1171faf26c73d5cd279b2ad83b789d363e544
lulizhou <1002359548@qq.com>
2015-09-07 17:09:32 +0800

    添加POst页面

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
 # -*- coding:utf-8 -*-
 from datetime import datetime
-from flask import redirect, url_for, render_template, session, request, flash
+from flask import redirect, url_for, render_template, session, request, flash, abort, current_app
 from . import main
-from flask.ext.login import current_user
+from flask.ext.login import current_user, login_required
 from app import db
 from .forms import NameForm, PostForm
 from ..models import User, Role, Permission, Post
@@ -18,11 +18,23 @@ def index():
                     author=current_user._get_current_object())
         db.session.add(post)
         return redirect(url_for('.index'))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template("index.html", form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.last_modified.desc()).paginate(page,
+                                                                     per_page=current_app.config[
+                                                                         'FLASKY_POSTS_PER_PAGE'],
+                                                                     error_out=False)
+    posts = pagination.items
+    return render_template("index.html", form=form, posts=posts, pagination=pagination)
+
+
+@main.route('/post/<int:id>', methods=['GET', 'POST'])
+def post(id):
+    post = Post.query.filter_by(id=id).first()
+    return render_template('post.html', post=post)
 
 
 @main.route('/delete', methods=['GET', 'POST'])
+@login_required
 def delete():
     if request.args is None:
         flash("参数错误！")
@@ -36,3 +48,22 @@ def delete():
         else:
             flash("你不能删！")
         return redirect(url_for('.index'))
+
+
+@main.route('/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    post = Post.query.get_or_404(id)
+    if current_user != post.author and not current_user.can(Permission.ADMINISTER):
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.body = form.body.data
+        post.last_modified = datetime.utcnow()
+        db.session.add(post)
+        flash("The post has been Update!")
+        return redirect(url_for('.index'))
+    form.title.data = post.title
+    form.body.data = post.body
+    return render_template('edit_post.html', form=form)
--- app/models.py
+++ app/models.py
@@ -118,6 +118,7 @@ class Post(db.Model):
     title = db.Column(db.String(64), index=True)
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    last_modified = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     body_html = db.Column(db.Text)
 
@@ -127,4 +128,20 @@ class Post(db.Model):
                         'i', 'li', 'ol', 'pre', 'strong', 'ul', 'h1', 'h2', 'h3', 'p']
         target.body_html = bleach.linkify(bleach.clean(markdown(value, output_format='html'),
                                                        tags=alowed_tages, strip=True))
+    @staticmethod
+    def generate_fake(count=100):
+        from random import seed, randint
+        import forgery_py
+        seed()
+        user_count = User.query.count()
+        for i in range(count):
+            time = forgery_py.date.date(True)
+            u = User.query.offset(randint(0, user_count-1)).first()
+            p = Post(body=forgery_py.lorem_ipsum.sentences(randint(1, 3)),
+                     timestamp=time,
+                     last_modified=time,
+                     title=forgery_py.lorem_ipsum.title(),
+                     author=u)
+            db.session.add(p)
+            db.session.commit()
 db.event.listen(Post.body, 'set', Post.on_changed_body)
