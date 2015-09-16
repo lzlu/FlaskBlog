8f3f04745a3b54b43379d89f810748445f626f9c
lulizhou <1002359548@qq.com>
2015-09-17 00:00:22 +0800

    add config.ini

--- config.py
+++ config.py
@@ -7,11 +7,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    RECAPTCHA_PUBLIC_KEY = '6LeYIbsSAAAAACRPIllxA7wvXjIE411PfdB2gt2J'
+    RECAPTCHA_PRIVATE_KEY = '6LeYIbsSAAAAAJezaIq3Ft_hSTo0YtyeFG-JgRtu'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASKY_ADMIN = '1002359548@qq.com'
-
+    FLASKY_POSTS_PER_PAGE = 10
     @staticmethod
     def init_app(app):
         pass
@@ -24,7 +26,7 @@ class DevelopmentConfig(Config):
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:121224@localhost/blog"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://rpvea7nkuothm1c9:121224@rdsm3amreurv2ae.mysql.rds.aliyuncs.com:3306/rpvea7nkuothm1c9"
 
 
 class TestingConfig(Config):
