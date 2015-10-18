12a5400e361c7099237569c7eaa6e4f7b7024500
lulizhou <1002359548@qq.com>
2015-10-18 21:33:57 +0800

    新增go2top按钮

---
+++ app/static/js/scroll2Top.js
@@ -0,0 +1,39 @@
+(function($,window) {
+    function scroll2Top(){}
+
+    scroll2Top.prototype={
+        init:function(){
+            if(this.nowScroll()>=1000){
+                this.dom();
+                this.animate();
+            }else if (this.nowScroll()<=200){
+                this.remove();
+            }
+        },
+        nowScroll:function(){
+            return $(document).scrollTop();
+        },
+        dom:function(){
+            if(!document.getElementById('go2top')){
+                 $('body').append( '<div id="go2top"></div>');
+            }
+
+        },
+        animate:function(){
+            var _this = this;
+            $('#go2top').on('click',function(){
+                $('body').animate({'scrollTop':0},function(){
+                _this.remove();
+            });
+            });
+
+        },
+        remove:function(){
+            $('#go2top').remove();
+        }
+    }
+    var scroll = new scroll2Top();
+    $(window).scroll(function(){
+        scroll.init();
+    });
+})(jQuery,window);
